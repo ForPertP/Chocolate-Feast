8f35f479a0db9d41d6b131399b33d98d8082b8fd
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-01 20:30:49 +0900

    Update Chocolate-Feast.cpp

--- Chocolate-Feast.cpp
+++ Chocolate-Feast.cpp
@@ -1,1 +1,16 @@
+int chocolateFeast(int n, int c, int m)
+{
+    int result = 0;
+    int chocolate = n / c;
+    int wrapper = 0;
 
+    while (chocolate > 0)
+    {
+        result += chocolate;
+        wrapper += chocolate;
+        chocolate = wrapper / m;
+        wrapper = wrapper % m;
+    }
+    
+    return result;
+}
