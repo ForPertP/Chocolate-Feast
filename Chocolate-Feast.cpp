9efed3457a5ca4f7c736344c83909a74d2a4b642
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-11 08:56:42 +0900

    Update Chocolate-Feast.cpp

--- Chocolate-Feast.cpp
+++ Chocolate-Feast.cpp
@@ -1,3 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'chocolateFeast' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER c
+ *  3. INTEGER m
+ */
+
 int chocolateFeast(int n, int c, int m)
 {
     int result = 0;
