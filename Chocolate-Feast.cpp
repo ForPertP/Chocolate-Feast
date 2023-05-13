bcf05e3f9a0fef753d644dae48cbeba0d56fc974
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-13 11:18:43 +0900

    Update Chocolate-Feast.cpp

--- Chocolate-Feast.cpp
+++ Chocolate-Feast.cpp
@@ -32,3 +32,35 @@ int chocolateFeast(int n, int c, int m)
     
     return result;
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int c = stoi(first_multiple_input[1]);
+
+        int m = stoi(first_multiple_input[2]);
+
+        int result = chocolateFeast(n, c, m);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
